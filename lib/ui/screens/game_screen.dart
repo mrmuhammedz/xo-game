c3a75cedcde3283e84af8f9da8548d606be6f6ba
Mr_Muhammed <mahmmoudmohammed94@gmail.com>
2026-07-21 23:37:56 +0300

    Update GameScreen UI structure

--- lib/ui/screens/game_screen.dart
+++ lib/ui/screens/game_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xo_game/ui/utils/app_colors.dart';
+import 'package:xo_game/ui/utils/app_style.dart';
 
 class GameScreen extends StatelessWidget {
   static final String routeName = "game screen";
@@ -26,6 +27,45 @@ class GameScreen extends StatelessWidget {
   Widget scaffoldBodyBuilder() {
     return Padding(
       padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: .center,
+          children: [
+            SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsetsGeometry.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadiusGeometry.circular(44),
+              ),
+              child: Text(
+                "00:00",
+                textAlign: .center,
+                style: AppStyle.black32semiBold,
+              ),
+            ),
+            SizedBox(height: 32),
+            Text("Player 1’s Turn", style: AppStyle.white36bold),
+            SizedBox(height: 24),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsetsGeometry.symmetric(
+                  horizontal: 13,
+                  vertical: 22,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadiusGeometry.circular(44),
+                ),
+                // child: ,
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
