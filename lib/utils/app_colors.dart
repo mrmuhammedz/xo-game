78856e1d53f72a5296af19e697945744a0e4bad7
Mr_Muhammed <mahmmoudmohammed94@gmail.com>
2026-07-25 06:42:09 +0300

    Move utils/ from ui/ to lib/ Remove grey and add rose and green to AppColors Remove grey22 and add white32semiBold text style to AppStyle Update imports in app_style.dart

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const Color white = Colors.white;
-  static const Color grey = Colors.white70;
   static const Color cyan = Color(0xFF00D2FF);
+  static const Color rose = Color(0xccf77181);
+  static const Color green = Color(0xcc9fe961);
   static const Color blue = Color(0xFF3A7BD5);
   static const Color black = Colors.black;
 }
